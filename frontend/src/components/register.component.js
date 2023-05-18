54b07e7e790d3d50f932e3098f04aac32cbbf110
Valdone Zabutkaite <valdonezabutkaite@MacBook-Pro-von-Valdone.local>
2023-05-18 19:01:25 +0200

    authentication frontend

--- frontend/src/components/register.component.js
+++
@@ -1,6 +0,0 @@
-import Form from "react-validation/build/form";
-import Input from "react-validation/build/input";
-import CheckButton from "react-validation/build/button";
-
-import { isEmail } from "validator";
-
